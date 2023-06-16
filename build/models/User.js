3d9b7200fa1e84eb5b70b87947155a62cc25b1a7
paulolopest <paulotarso-lopes@hotmail.com>
2023-06-16 17:54:33 -0300

    test

--- build/models/User.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ROLE = void 0;
-var ROLE;
-(function (ROLE) {
-    ROLE["NORMAL"] = "Normal";
-    ROLE["ADMIN"] = "Administrator";
-})(ROLE = exports.ROLE || (exports.ROLE = {}));
-//# sourceMappingURL=User.js.map
--- build/tests/teste.test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-test("Teste", () => {
-    const returned = "oi";
-    expect(returned).toEqual("oi");
-});
-//# sourceMappingURL=teste.test.js.map
