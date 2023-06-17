abd3431da3b08d9cd6495a9fd907b57be731fbc8
paulolopest <paulotarso-lopes@hotmail.com>
2023-06-17 18:26:37 -0300

    .

--- src/services/HashManager.ts
+++ src/services/HashManager.ts
@@ -1,17 +1,20 @@
-import * as bcrypt from "bcryptjs"
-import dotenv from "dotenv"
+import * as bcrypt from 'bcryptjs';
+import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 
 export class HashManager {
-    generateHash = async(password: string): Promise <string> => {
-        const rounds = Number(process.env.BYCRYP_COST)
-        const salt = await bcrypt.genSalt(rounds)
+	generateHash = async (password: string): Promise<string> => {
+		const rounds = Number(process.env.BYCRYPT_COST);
+		const salt = await bcrypt.genSalt(rounds);
 
-        return bcrypt.hash(password, salt)
-    }
+		return bcrypt.hash(password, salt);
+	};
 
-    compareHash = (password: string, cypherPassword: string): Promise<boolean> => {
-        return bcrypt.compare(password, cypherPassword)
-    }
+	compareHash = (
+		password: string,
+		cypherPassword: string
+	): Promise<boolean> => {
+		return bcrypt.compare(password, cypherPassword);
+	};
 }
