8519fc60b433d8ce0171e9383f55e31edda10789
paulolopest <paulotarso-lopes@hotmail.com>
2023-07-12 19:08:34 -0300

    Testing

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { cartRouter } from './Router/Cart/CartRouter';
 import { cardRouter } from './Router/Card/CardRouter';
 import { productRouter } from './Router/Product/ProductRouter';
 import { favoriteProductRouter } from './Router/Favorites/FavoriteProductRouter';
+import { userController } from './Models/Classes';
 
 const port = process.env.PORT || 3000;
 const app: Express = express();
@@ -23,3 +24,13 @@ app.use(cartRouter);
 app.use(cardRouter);
 app.use(productRouter);
 app.use(favoriteProductRouter);
+
+userRouter.post('/signup', userController.signup);
+userRouter.post('/login', userController.login);
+userRouter.post('/token/validate-token', userController.validateToken);
+
+userRouter.get('/profile', userController.getProfile);
+
+userRouter.put('/profile/edit', userController.editProfileName);
+
+userRouter.delete('/user/profile', userController.deleteUser);
