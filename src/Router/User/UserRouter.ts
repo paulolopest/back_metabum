9384b9e2b22286ce9579e70fe870fc9021c0ebb1
paulolopest <paulotarso-lopes@hotmail.com>
2023-07-13 11:16:46 -0300

    Testing

--- src/Router/User/UserRouter.ts
+++ src/Router/User/UserRouter.ts
@@ -1,5 +1,18 @@
 import express, { Router } from 'express';
-import { userController } from '../../Models/Classes';
+import { UserData } from '../../Data/User/UserData';
+import { IdGenerator } from '../../Services/IdGenerator';
+import { HashManager } from '../../Services/HashManager';
+import { Authenticator } from '../../Services/Authenticator';
+import { UserBusiness } from '../../Business/User/UserBusiness';
+import { UserController } from '../../Controller/User/UserController';
+
+const userBusiness: UserBusiness = new UserBusiness(
+	new Authenticator(),
+	new HashManager(),
+	new IdGenerator(),
+	new UserData()
+);
+const userController: UserController = new UserController(userBusiness);
 
 export const userRouter: Router = express.Router();
 
