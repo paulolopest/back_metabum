9f00b348d2c9627705da07afc85b12ad08b36372
paulolopest <paulotarso-lopes@hotmail.com>
2023-07-13 18:06:12 -0300

    .

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import cors from 'cors';
 import express from 'express';
+import { productController } from './Models/Classes';
 // import { userRouter } from './Router/User/UserRouter';
 // import { cardRouter } from './Router/Card/CardRouter';
-// import { productRouter } from './Router/Product/ProductRouter';
+// import { app } from './Router/Product/ProductRouter';
 // import { favoriteProductRouter } from './Router/Favorites/FavoriteProductRouter';
-import cartRouter from './router/cart/cartrouter';
+// import cartRouter from './router/cart/cartrouter';
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -21,7 +22,21 @@ const server = app.listen(port, () => {
 //Routes
 
 // app.use(userRouter);
-app.use(cartRouter);
+// app.use(cartRouter);
 // app.use(cardRouter);
 // app.use(productRouter);
 // app.use(favoriteProductRouter);
+
+app.post('/insertProduct', productController.insertProduct);
+app.post('/:productId/add-description', productController.addDescription);
+
+app.get('/products', productController.getProducts);
+app.get('/:productId/description', productController.getDescriptions);
+app.get('/products/:id', productController.getProductById);
+
+app.put('/product/:productId/edit', productController.editPrice);
+app.put('/product/edit-quantity/:productId', productController.editQuantity);
+app.put('/:descriptionId/edit-description', productController.editDescription);
+
+app.delete('/product/:productId', productController.deleteProduct);
+app.delete('/:descriptionId/delete', productController.deleteDescription);
